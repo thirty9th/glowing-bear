0af97f0d5d9e99a81a14549106a5b3d4d78de35a
Dillon Fisher <thirty9th@gmail.com>
2013-06-24 15:00:59 -0500

    Added loading functions for listview, began implementing file IO

--- src/com/example/minecraftapp/MainActivity.java
+++ src/com/example/minecraftapp/MainActivity.java
@@ -7,7 +7,13 @@
 
 package com.example.minecraftapp;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +27,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
@@ -30,7 +37,8 @@ public class MainActivity extends Activity
 	static final String FILENAME_WORKSHEETS = "worksheets";
 	
 	// Views to be instantiated
-	ListView list;
+	ListView worksheetListView;
+	TextView worksheetTitleText;
 	
 	// Globals
 	ItemDataManager itemManager;
@@ -46,7 +54,8 @@ public class MainActivity extends Activity
 		setContentView(R.layout.activity_main);
 		
 		// Instantiate views
-		list = (ListView)findViewById(R.id.list_worksheets);
+		worksheetListView = (ListView)findViewById(R.id.list_worksheets);
+		worksheetTitleText = (TextView)findViewById(R.id.text_main_titlebar);
 		
 		// Create the item data manager and pull items from XML file
 		itemManager = new ItemDataManager();
@@ -65,23 +74,20 @@ public class MainActivity extends Activity
 			Toast.makeText(this, "ERROR: IOException", Toast.LENGTH_SHORT).show();
 		}
 		
+		// Set up worksheet storage file if this is the first time entering the app
+		setupWorksheetFile();
+		
 		// Populate the list of worksheets
 		worksheetList = new ArrayList<String>();
-//		loadWorksheets();
+		int worksheetCount = readWorksheetFile();
+		if (worksheetCount == 0) worksheetList.add("No worksheets found");
 		
-		// Test custom listview
-		List<HashMap<String,String>> hashList = new ArrayList<HashMap<String,String>>();
-		for(int i = 0; i < 10; i++)
-		{
-            HashMap<String, String> hm = new HashMap<String,String>();
-            hm.put("name", String.valueOf(i));
-            hashList.add(hm);
-        }
-		String[] from = {"name"};
-		int[] to = {R.id.text_worksheet_name};
-		SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), hashList, R.layout.listview_item_worksheets, from, to);
-		list.setAdapter(adapter);
+		// Now add all the worksheets to the listview for display
+		loadWorksheetListview();
 		
+		// Update count of worksheets displayed on the titlebar
+		worksheetTitleText.setText("Worksheets (" + worksheetCount + ")");
+
 		// Test display of all items
 //		int count = 0;
 //		int numRecipes;
@@ -106,6 +112,75 @@ public class MainActivity extends Activity
 	public void onClickAddWorksheet(View v)
 	{
 		Toast.makeText(getBaseContext(), "Clicked add worksheet.", Toast.LENGTH_SHORT).show();
+	}
+	
+	// Sets up the worksheet file for future writing and reading
+	private void setupWorksheetFile()
+	{
+		File f = new File(FILENAME_WORKSHEETS);
+		try
+		{
+			f.createNewFile();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			Toast.makeText(this, "ERROR: IOException", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	// Reads worksheets from stored data, if any exist
+	// Returns: number of worksheets read
+	private int readWorksheetFile()
+	{
+		int count = 0;
+		try
+		{
+			// Open file input and wrappers: ends up in buffered reader
+			FileInputStream fis = new FileInputStream(FILENAME_WORKSHEETS);
+			DataInputStream dis = new DataInputStream(fis);
+			BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+			
+			// Read file by line
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				worksheetList.add(line);
+				count++;
+			}
+			
+			// Close reader
+			dis.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			Toast.makeText(this, "ERROR: FileNotFoundException", Toast.LENGTH_SHORT).show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			Toast.makeText(this, "ERROR: IOException", Toast.LENGTH_SHORT).show();
+		}
+		
+		return count;
+	}
+	
+	// Populates the listview from the list of worksheets
+	// Note: uses hashmaps for extensibility... may add custom worksheet icons later
+	private void loadWorksheetListview()
+	{
+		List<HashMap<String, String>> hashList = new ArrayList<HashMap<String, String>>();
+		for(int i = 0; i < worksheetList.size(); i++)
+		{
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("name", worksheetList.get(i));
+            hashList.add(hm);
+        }
+		String[] from = {"name"};
+		int[] to = {R.id.text_worksheet_name};
+		SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), hashList, R.layout.listview_item_worksheets, from, to);
+		worksheetListView.setAdapter(adapter);
 	}
 
 	// Called when the user hits the menu softkey
